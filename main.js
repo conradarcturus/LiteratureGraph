31ad21112158aab5b7d4697b995a2e7ef6398495
A. Conrad Nied <anied@cs.washington.edu>
2015-01-30 01:49:01 -0800

    Can change labels now

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var width = 960,
 var color = d3.scale.category20();
 
 var force = d3.layout.force()
-    .gravity(.05)
+    .gravity(.1)
     .charge(-300)
     .distance(100)
     .size([width, height])
@@ -29,18 +29,105 @@ restart();
 // Add Interactions
 
 
-function editNode() {
-  console.log("EditNode");
+function editNode(node, d) {
+  var p = node.parentNode;
+  var node_d3 = d3.select(node);
+  console.log(d.order);
+  console.log("editNode", node);
+
+  // inject a HTML form to edit the content here...
+
+  // bug in the getBBox logic here, but don't know what I've done wrong here;
+  // anyhow, the coordinates are completely off & wrong. :-((
+  var xy = node.getBBox();
+  var p_xy = p.getBBox();
+
+  xy.x -= p_xy.x;
+  xy.y -= p_xy.y;
+
+  var displayText = node_d3.select("text");
+  var field = "name";
+
+  var frm = node_d3.append("foreignObject")
+    .attr("id", "editbox");
+
+  var inp = frm
+    .attr("width", xy.width)
+    .attr("height", xy.height)
+    .append("xhtml:form")
+      .append("input")
+        .attr("value", function() {
+          // nasty spot to place this call, but here we are sure that the <input> tag is available
+          // and is handily pointed at by 'this':
+          node.focus();
+
+          return d[field];
+        })
+        .attr("style", "width: 294px;")
+        .style("background-color", color(d.group))
+        .on("mousedown", function(d) {
+          d3.event.stopPropagation();
+        })
+        .on("dblclick", function(d) {
+          d3.event.stopPropagation();
+        })
+        // make the form go away when you jump out (form looses focus) or hit ENTER:
+        .on("blur", function() {
+          console.log("blur", node);
+
+          var txt = inp.node().value;
+
+          displayText.text(txt);
+          nodes[d.order][field] = txt;
+
+          // Note to self: frm.remove() will remove the entire <g> group! Remember the D3 selection logic!
+          try {
+            node_d3.select("#editbox").remove();
+          } catch (e) {
+            // do nothing, super bad but this was throwing strange errors.
+          }
+        })
+        .on("keypress", function() {
+          console.log("keypress", node, d3.event.keyCode);
+
+          // IE fix
+          if (!d3.event)
+              d3.event = window.event;
+
+          var e = d3.event;
+          if (e.keyCode == 13)
+          {
+            if (typeof(e.cancelBubble) !== 'undefined') // IE
+              e.cancelBubble = true;
+            if (e.stopPropagation)
+              e.stopPropagation();
+            e.preventDefault();
+
+            var txt = inp.node().value;
+
+            d[field] = txt;
+            displayText.text(txt);
+            nodes[d.order][field] = txt;
+
+            // odd. Should work in Safari, but the debugger crashes on this instead.
+            // Anyway, it SHOULD be here and it doesn't hurt otherwise.
+            try{
+              node_d3.select("#editbox").remove();
+            } catch (e) {
+            // do nothing, super bad but this was throwing strange errors.
+            }
+          }
+        });
 }
 
 function mousemove() {
   // nothing
 }
 
-nextNodeID = 1;
+var nextNodeID = 0;
 function addNode() {
   var point = d3.mouse(this),
-      node = {x: point[0], y: point[1], group:1, name: "Node" + nextNodeID},
+      node = {x: point[0], y: point[1], group:1, name: "Node" + nextNodeID, order:nextNodeID},
       n = nodes.push(node);
     nextNodeID++;
 
@@ -74,10 +161,10 @@ function restart() {
 
   node = node.data(nodes);
 
-  node.enter().append("g")
+  freshnode = node.enter().append("g")
       .attr("class", "node")
       .on("dblclick", function(d) {
-          editNode();
+          editNode(this, d);
         })
       .on("mousedown", function(d) {
         d3.event.stopPropagation();
@@ -86,13 +173,13 @@ function restart() {
         })
       .call(force.drag);
 
-  node.append("rect")
+    freshnode.append("rect")
       .attr("class", "node")
       .attr("width", 100)
       .attr("height", 20)
       .style("fill", function(d) { return color(d.group); });
-
-  node.append("text")
+    
+    freshnode.append("text")
       .attr("dy", "1em")
       .style("stroke", "none")
       .style("fill", "black")
@@ -102,51 +189,24 @@ function restart() {
 }
 
 
-// Populate Force Network
-var graphe;
+// Import Data
 
 d3.json("data.json", function(error, graph) {
   // Add each node from the graph into the view
+
   graph.nodes.forEach(function(nodeinfo) {
-    var node = {x: width/2, y: height/2, group:nodeinfo.group, name:nodeinfo.name},
+    var node = {x: width/2, y: height/2, group:nodeinfo.group, name:nodeinfo.name, order:nextNodeID},
         n = nodes.push(node);
+    nextNodeID++;
   })
 
   // Connect the nodes
-
-
-  graphe = graph;
+  graph.links.forEach(function(linkinfo) {
+    // console.log(linkinfo.source, linkinfo.target, nodes);
+    node_source = nodes[linkinfo.source];
+    node_target = nodes[linkinfo.target];
+    links.push({source: node_source, target: node_target, value: linkinfo.value});
+  })
 
   restart();
-
-
-  // // add links to any nearby nodes
-  // nodes.forEach(function(target) {
-  //   var x = target.x - node.x,
-  //       y = target.y - node.y;
-  //   if (Math.sqrt(x * x + y * y) < 30) {
-  //     links.push({source: node, target: target});
-  //   }
-
-
-
-
-  // var link = svg.selectAll(".link")
-  //     .data(graph.links)
-  //   .enter().append("line")
-  //     .attr("class", "link")
-  //     .style("stroke-width", function(d) { return Math.sqrt(d.value); });
-
-  // var nodes = svg.selectAll(".node")
-  //     .data(graph.nodes)
-  //   .enter().append("g")
-  //     .attr("class", "node")
-  //     .call(force.drag);
-
-  // nodes.append("rect")
-  //     .attr("class", "node")
-  //     .attr("width", 100)
-  //     .attr("height", 20)
-  //     .style("fill", function(d) { return color(d.group); })
-
 });
