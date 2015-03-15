423fa10455933845bd16d178d91a20f9fcb1c7cd
A. Conrad Nied <anied@cs.washington.edu>
2015-03-15 15:44:27 -0700

    Added more papers

--- papers/cheng2010developing_files/epdf.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  try { window['readcube-request-ua'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'; } catch(e) {}
-  var script = document.createElement('script');
-  script.async = true;
-  script.type = 'text/javascript';
-  script.src = 'https://content-assets.readcube.com/assets/wiley_epdf-223fced312d7a982a8c2bcb06749fb5b.js';
-  document.getElementsByTagName('head').item(0).appendChild(script);
-})();
